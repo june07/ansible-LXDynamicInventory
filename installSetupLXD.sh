6e89aa2f24ecf2f3bb3acb5d38a4a9244d3ce251
Will Smith <will@onezerohosting.com>
2016-10-03 08:49:31 -0700

    .

--- installSetupLXD.sh
+++ installSetupLXD.sh
@@ -10,41 +10,40 @@ if [[ $(echo $CONTAINER | grep -i server) || $(echo $CONTAINER | grep -i contain
 fi
 sudo add-apt-repository ppa:ubuntu-lxc/lxd-stable -y > /dev/null 2>&1
 sudo apt-get -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confnew" update -qq
-#sudo apt-get -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confnew" dist-upgrade -qq
-sudo apt-get -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confnew" install python lxd -qq
+sudo apt-get -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confnew" install -qq python lxd
 if [[ ! $(echo $CONTAINER | grep -i container) ]]; then
-  sudo apt-get -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confnew" install apt-cacher-ng -qq
+  sudo apt-get -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confnew" install -qq apt-cacher-ng
 fi
 
-#sudo newgrp lxd
-sudo lxd init --auto --network-address 10.202.${NETWORK}.1 --network-port 8443 --trust-password=$PASSWORD
-
 if [[ ! $(echo $CONTAINER | grep -i container) ]]; then
-sudo systemctl stop lxd-bridge
-sudo systemctl --system daemon-reload
-sudo su -c 'cat <<EOF > /etc/default/lxd-bridge
-USE_LXD_BRIDGE="true"
-LXD_BRIDGE="lxdbr0"
-UPDATE_PROFILE="true"
-LXD_CONFILE=""
-LXD_DOMAIN="lxd"
-LXD_IPV4_ADDR="10.202.'$NETWORK'.1"
-LXD_IPV4_NETMASK="255.255.255.0"
-LXD_IPV4_NETWORK="10.202.'$NETWORK'.1/24"
-LXD_IPV4_DHCP_RANGE="10.202.'$NETWORK'.2,10.202.'$NETWORK'.254"
-LXD_IPV4_DHCP_MAX="252"
-LXD_IPV4_NAT="true"
-LXD_IPV6_ADDR=""
-LXD_IPV6_MASK=""
-LXD_IPV6_NETWORK=""
-LXD_IPV6_NAT="false"
-LXD_IPV6_PROXY="false"
-EOF
-'
-sudo systemctl enable lxd-bridge
-sudo systemctl start lxd-bridge
-
-lxc config set core.https_address [::]:8443
-lxc config set core.trust_password $PASSWORD
+  sudo systemctl stop lxd-bridge
+  sudo systemctl --system daemon-reload
+  sudo su -c 'cat <<EOF > /etc/default/lxd-bridge
+  USE_LXD_BRIDGE="true"
+  LXD_BRIDGE="lxdbr0"
+  UPDATE_PROFILE="true"
+  LXD_CONFILE=""
+  LXD_DOMAIN="lxd"
+  LXD_IPV4_ADDR="10.202.'$NETWORK'.1"
+  LXD_IPV4_NETMASK="255.255.255.0"
+  LXD_IPV4_NETWORK="10.202.'$NETWORK'.1/24"
+  LXD_IPV4_DHCP_RANGE="10.202.'$NETWORK'.2,10.202.'$NETWORK'.254"
+  LXD_IPV4_DHCP_MAX="252"
+  LXD_IPV4_NAT="true"
+  LXD_IPV6_ADDR=""
+  LXD_IPV6_MASK=""
+  LXD_IPV6_NETWORK=""
+  LXD_IPV6_NAT="false"
+  LXD_IPV6_PROXY="false"
+  EOF'
+  sudo systemctl enable lxd-bridge
+  sudo systemctl start lxd-bridge
+  
+  lxc config set core.https_address [::]:8443
+  lxc config set core.trust_password $PASSWORD
 fi
+
+sudo newgrp lxd << SCRIPT
+  sudo lxd init --auto --network-address 10.202.${NETWORK}.1 --network-port 8443 --trust-password=$PASSWORD
+SCRIPT
 
