b4c0028e264d906204a5a7a92bc36e5ae77ecc47
Will Smith <will@onezerohosting.com>
2016-10-05 22:23:37 -0700

    .

--- test.sh
+++ test.sh
@@ -12,7 +12,7 @@ LXD_BRIDGE_IP=$(ip addr show dev lxdbr0 scope global | grep inet | grep -v inet6
 # Update hosts file to resolv hostname of LXD container.
 sudo cp /etc/resolv.conf /tmp/.resolv.conf.backup-$(date +%s)
 sudo perl -0 -pi -e "s/nameserver /nameserver $LXD_BRIDGE_IP\nnameserver /" /etc/resolv.conf
-echo -e "$(sudo lxc list ubuntu-adi-test-lxdserver -c4 | grep eth0 | cut -d' ' -f2)\tubuntu-adi-test-lxdserver" | sudo tee -a /etc/hosts
+echo -e "\n$(sudo lxc list ubuntu-adi-test-lxdserver -c4 | grep eth0 | cut -d' ' -f2)\tubuntu-adi-test-lxdserver" | sudo tee -a /etc/hosts
 
 # Execute tests.
 source ./hacking/env-setup
